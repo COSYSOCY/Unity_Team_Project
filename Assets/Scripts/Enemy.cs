91aea0cd9b7c30d553d241769b6724b9af23d8d3
UnityCJK <cjk225221@gmail.com>
2022-03-17 18:01:59 +0900

    NavUpdate

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,37 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public PlayerStatus playerStatus;
     private Transform target;
-    [SerializeField] private int hp = 100;
-    private float enemyDmg = 10;
+    private int hp = 100;
+    private int enemyDmg = 20;
     private float atkDelay;
     private float atkCoolTime;
-    private float moveSpeed = 5f;
+    private float moveSpeed = 2f;
     private bool isDead;
 
-    private void Start()
+    NavMeshAgent nav;
+
+    void Start()
     {
+        nav = GetComponent<NavMeshAgent>();
         target = GameObject.Find("Player").GetComponent<Transform>();
+        StartCoroutine(UpdateEnemy());
     }
-    private void Update()
+
+    void NavEnemy(Vector3 _target)
     {
-        EnemyTarget();
+        nav.SetDestination(_target);
     }
 
-    void EnemyTarget()
+    IEnumerator UpdateEnemy()
     {
-
-
-        Vector3 dir = target.transform.position - gameObject.transform.position;
-        Debug.Log(dir);
-        transform.position = Vector3.MoveTowards(transform.position, dir, Time.deltaTime * moveSpeed);
-        //transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
-        //transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime);
+        while (!isDead)
+        {
+            StartCoroutine(EnemyTarget());
+            StartCoroutine(MoveEnemy());
+            yield return new WaitForSeconds(0.5f);
+        }
     }
+    IEnumerator EnemyTarget()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime);
+        yield return new WaitForSeconds(0.2f);
+    }
+    IEnumerator MoveEnemy()
+    {
+        nav.speed = moveSpeed;
+        NavEnemy(target.position);
+        yield return null;
 
-
+    }
 }
