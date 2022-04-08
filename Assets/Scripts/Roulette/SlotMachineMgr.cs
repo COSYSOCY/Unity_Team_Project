583a94571b850b35ef358f635aae48d9dd8ac739
wolstar415 <95416658+wolstar415@users.noreply.github.com>
2022-04-08 18:59:50 +0900

    asd
    asd

--- Assets/Scripts/Roulette/SlotMachineMgr.cs
+++ Assets/Scripts/Roulette/SlotMachineMgr.cs
@@ -63,10 +63,11 @@ public class SlotMachineMgr : MonoBehaviour
     int[] answer = { 2, 3, 1 };
     IEnumerator StartSlot(int SlotBtnIndex)
     {       
-        for (int i = 0; i < (ItemCnt * (6 +SlotBtnIndex * 4)+ answer[SlotBtnIndex]) *2 ; i++)
+        //for (int i = 0; i < (ItemCnt * (6 +SlotBtnIndex * 4)+ answer[SlotBtnIndex]) *2 ; i++)
+        for (int i = 0; i < 1000 ; i++)
         {
             SlotSkillObject[SlotBtnIndex].transform.localPosition -= new Vector3(0, 50f, 0);
-            if(SlotSkillObject[SlotBtnIndex].transform.localPosition.y < 50f)
+            if(SlotSkillObject[SlotBtnIndex].transform.localPosition.y < 70f)
             {
                 SlotSkillObject[SlotBtnIndex].transform.localPosition += new Vector3(0, 300f, 0);
             }
